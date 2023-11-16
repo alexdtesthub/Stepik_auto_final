c68e784020c4ce60890ab690f0282ae2b71c86f5
alex_d <131162935+alexdtesthub@users.noreply.github.com>
2023-11-16 14:49:12 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -15,9 +15,9 @@ from .pages.product_page import ProductPage
                                   # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
                                   # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
                                   # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  #  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  # pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
                                   # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  #"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
+                                  # "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"
                                  ])
 def test_guest_can_add_product_to_basket(browser, link):
     pages = ProductPage(browser, link)
